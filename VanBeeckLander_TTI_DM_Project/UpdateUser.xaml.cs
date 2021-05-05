a1f619a6a598f1f0c5ca0f7241df5f4903e50565
LanderVanBeeck <Landervanbeeck@hotmail.com>
2021-05-05 12:55:37 +0200

    Lay-out + link naar database

---
+++ VanBeeckLander_TTI_DM_Project/UpdateUser.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VanBeeckLander_TTI_DM_Project
+{
+    /// <summary>
+    /// Interaction logic for UpdateUser.xaml
+    /// </summary>
+    public partial class UpdateUser : Window
+    {
+        public UpdateUser()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUpdateUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
